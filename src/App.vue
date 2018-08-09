53b6afddb7e7c2ecf41ec8059e064f9eda8ea33d
Forrest Wilson <forrest.wilson@me.com>
2018-08-09 15:23:15 +1200

    JS animation hooks

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,9 @@
                 <br><br>
                 <button class="btn btn-primary" @click="show = !show">Show Alert</button>
                 <br><br>
+
+                <!-- CSS Animations and Transitions -->
+
                 <transition :name="alertAnimation">
                     <div class="alert alert-info" v-show="show">This is some info</div>
                 </transition>
@@ -27,6 +30,27 @@
                     <div class="alert alert-info" v-if="show" key="info">This is some info</div>
                     <div class="alert alert-warning" v-else key="warning">This is some warning</div>
                 </transition>
+
+                <hr>
+
+                <!-- JS Animations and Transitions -->
+
+                <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+
+                <br><br>
+
+                <transition 
+                    @before-enter="beforeEnter"
+                    @enter="enter"
+                    @after-enter="afterEnter"
+                    @enter-cancelled="enterCancelled"
+                    
+                    @before-leave="beforeLeave"
+                    @leave="leave"
+                    @after-leave="afterLeave"
+                    @leave-cancelled="leaveCancelled">
+                    <div style="width: 100px; height: 100px; background-color: lightgreen;" v-if="load"></div>
+                </transition>
             </div>
         </div>
     </div>
@@ -36,8 +60,38 @@
     export default {
         data() {
             return {
-                show: true,
-                alertAnimation: 'fade'
+                show: false,
+                alertAnimation: 'fade',
+                load: true
+            }
+        },
+        methods: {
+            beforeEnter (el) {
+                console.log('before enter');
+            },
+            enter (el, done) {
+                console.log('enter');
+                done();
+            },
+            afterEnter (el) {
+                console.log('after enter');
+            },
+            enterCancelled (el) {
+                console.log('enter cancelled');
+            },
+
+            beforeLeave (el) {
+                console.log('before leave');
+            },
+            leave (el, done) {
+                console.log('leave');
+                done();
+            },
+            afterLeave () {
+                console.log('after leave');
+            },
+            leaveCancelled () {
+                console.log('leave cancelled');
             }
         }
     }
