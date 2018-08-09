e651182c8fb3e85d01016351507741a2a227665a
Forrest Wilson <forrest.wilson@me.com>
2018-08-09 12:43:45 +1200

    ???

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
 
-                <transition name="slide">
+                <transition name="slide" type="animation">
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
             </div>
@@ -49,11 +49,12 @@
     }
 
     .slide-enter {
-        
+        opacity: 0;
     }
 
     .slide-enter-active {
         animation: slide-in 1s ease-out forwards;
+        transition: opacity 0.5s;
     }
 
     .slide-leave {
@@ -62,6 +63,8 @@
 
     .slide-leave-active {
         animation: slide-out 1s ease-out forwards;
+        transition: opacity 3s;
+        opacity: 0;
     }
 
     @keyframes slide-in {
