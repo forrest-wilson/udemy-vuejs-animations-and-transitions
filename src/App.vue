6350cd966606c75ab278dfb193edde10ad3dd633
Forrest Wilson <forrest.wilson@me.com>
2018-08-09 15:25:00 +1200

    Telling Vue not to look for CSS in JS hooks

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,8 @@
                     @before-leave="beforeLeave"
                     @leave="leave"
                     @after-leave="afterLeave"
-                    @leave-cancelled="leaveCancelled">
+                    @leave-cancelled="leaveCancelled"
+                    :css="false">
                     <div style="width: 100px; height: 100px; background-color: lightgreen;" v-if="load"></div>
                 </transition>
             </div>
@@ -71,7 +72,7 @@
             },
             enter (el, done) {
                 console.log('enter');
-                done();
+                done(); // Required for JS hooks
             },
             afterEnter (el) {
                 console.log('after enter');
@@ -85,7 +86,7 @@
             },
             leave (el, done) {
                 console.log('leave');
-                done();
+                done(); // Required for JS hooks
             },
             afterLeave () {
                 console.log('after leave');
